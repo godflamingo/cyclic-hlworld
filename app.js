249c6e2fa6d94dbf53a82e74bbc5b55fd46fdbb3
godflamingo <43773423+godflamingo@users.noreply.github.com>
2023-02-04 15:09:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,16 +53,20 @@ app.get("/info", (req, res) => {
 });
 
 app.use(
-  "/app",
+  "/api",
   createProxyMiddleware({
     target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
-      // 请求中去除/app
-      "^/app": "/app",
+      // 请求中去除/api
+      "^/api": "/qwe",
     },
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
+    onProxyReq: function onProxyReq(proxyReq, req, res) {
+      // 我就打个log康康
+      console.log("-->  ", req.method, req.baseUrl, "->", proxyReq.host + proxyReq.path
+      );
+    },
   })
 );
 
